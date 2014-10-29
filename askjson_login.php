0b997eae93d7dc50777f5978f716e623e4c7af1a
ask1612 <akasun@rambler.ru>
2014-10-29 13:43:42 -0300

    Login

--- askjson_login.php
+++ askjson_login.php
@@ -4,7 +4,42 @@
  * 
  * askjson_login.php
  * @author ASK
- * 
- * 
+ * User login
  * 
  */
+
+
+require_once __DIR__.'/askjson_config.php';
+//Get JSON object
+$jsonString = filter_input(INPUT_POST,TAG_JSON);
+$jsonArr=json_decode($jsonString,true);
+//Feth user name and password 
+$username =trim($jsonArr[TAG_NAME]);//User name
+$userpwd=trim($jsonArr[TAG_PWD]);//Password
+ 
+
+/* Check user name and password.*/
+if(empty($username)||empty($userpwd)){//User name  or pssword is empty.
+    $response[TAG_SUCCESS] = 0;
+    $response[TAG_MESSAGE] = "Username and Password must not be empty";
+    die(json_encode($response));
+    }
+ else{//OK!User name and password is not empty. 
+    require_once __DIR__.'/askjson_connect.php';
+    $db = new DB_Connect();//Connect to databse. 
+    $result = $db->selectUser($username);//Search user.  
+    if(empty($result)){//User  is not found in MySql database.
+            $response[TAG_SUCCESS]= 0;
+            $response[TAG_MESSAGE] = 'User account  ' .$username. ' is not found!';
+            die(json_encode($response));    
+        }
+    else{ //A user with this name already exists in the database. 
+        $response[TAG_SUCCESS]= 1;
+        $response[TAG_MESSAGE] = '$username' ;
+        die(json_encode($response));
+        }    
+    }
+ //Parameter JSON_UNESCAPED_UNICODE is required to display cyrillic text
+ 
+
+
--- askjson_register.php
+++ askjson_register.php
@@ -34,14 +34,14 @@ if(empty($username)||empty($userpwd)){//User name  or pssword is empty.
             die(json_encode($response));    
             }
         else{
-            $response[TAG_SUCCESS]= 1;
+            $response[TAG_SUCCESS]= 0;
             $response[TAG_MESSAGE] = 'Cannot insert account ' .$result. ' in database.'
                     . 'Check the connection to the database.';
             die(json_encode($response));    
             }    
         }
     else{ //A user with this name already exists in the database.Enter another user name 
-        $response[TAG_SUCCESS]= 1;
+        $response[TAG_SUCCESS]= 0;
         $response[TAG_MESSAGE] = 'A user with  name '.$result.' already exists in the database.'
                 . 'Please enter another user name or Press button Login';
         die(json_encode($response));
